1555c34a3d7fc72828dc20a12f9632ec1edb181c
Ahmed-Gomaa <108756442+gAhmedg@users.noreply.github.com>
2024-04-27 10:07:08 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,22 +2,33 @@
 resource "aws_instance" "my_ec2" {
   ami             = "ami-04b70fa74e45c3917"  # Specify your AMI ID
   instance_type   = "t2.micro"  
-  subnet_id       = aws_subnet.my_subnet.id  
+  subnet_id       = aws_subnet.my_subnet.id 
+  security_groups = [aws_security_group.my_sg.id] 
   tags = {
     Name = "my-ec2-instance"
   }
 }
 
-# Create security group
+
+#####################################################################################
+# security_group
+#####################################################################################
+
 resource "aws_security_group" "my_sg" {
-  name        = "my-security-group"
-  description = "Allow inbound traffic on port 80"
+  name        = "allow_http"
+  description = "Allow http traffic "
   vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow-http"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.my_sg.id
+  cidr_ipv4         = aws_vpc.my_vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
